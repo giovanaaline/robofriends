0a3489fa185c2fe65b947d68202c3b164d090db9
Sistema TRIX <sistema@trixsys.com.br>
2018-12-21 13:23:03 -0200

    Até Aula 186: Componente CardList e App

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+{
+	id:1,
+	name: 'Giovana Aline Bruno',
+	username: 'Giovana',
+	email: 'giovana@savy.com.br'
+},
+{
+	id:2,
+	name: 'Debora Bagio',
+	username: 'Debora',
+	email: 'debora@savy.com.br'
+},
+{
+	id:3,
+	name: 'Jaqueline Souza',
+	username: 'Jaque',
+	email: 'suporte@savy.com.br'
+},
+{
+	id:4,
+	name: 'Reculos Neto',
+	username: 'Neto',
+	email: 'neto@savy.com.br'
+},
+{
+	id:5,
+	name: 'David Soares',
+	username: 'David',
+	email: 'david@savy.com.br'
+}
+]
