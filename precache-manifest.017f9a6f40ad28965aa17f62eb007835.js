8d3e79fe78be558bf61c4c87811ee9d0361ad6d3
Giovana Aline Bruno <giovana.aline@gmail.com>
2019-12-18 21:37:00 -0300

    Updates

---
+++ precache-manifest.017f9a6f40ad28965aa17f62eb007835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "95274ac8cd20e904eda0",
+    "url": "/robofriends/static/js/main.95274ac8.chunk.js"
+  },
+  {
+    "revision": "31d4fea6d9ada47839bc",
+    "url": "/robofriends/static/js/1.31d4fea6.chunk.js"
+  },
+  {
+    "revision": "95274ac8cd20e904eda0",
+    "url": "/robofriends/static/css/main.fd035c90.chunk.css"
+  },
+  {
+    "revision": "31d4fea6d9ada47839bc",
+    "url": "/robofriends/static/css/1.b1dabfab.chunk.css"
+  },
+  {
+    "revision": "0189343a96221d6cbc8680f653bb6bc7",
+    "url": "/robofriends/index.html"
+  }
+];
